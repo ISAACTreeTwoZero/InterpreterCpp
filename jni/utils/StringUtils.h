40b4d42e67580d15013c5dc6fd8912da9a49b00c
ISAAC300 <isaacsantosdsantos@gmail.com>
2017-03-04 09:11:14 -0400

    Create StringUtils.h

---
+++ jni/utils/StringUtils.h
@@ -0,0 +1,31 @@
+#ifndef STRINGUTILS_H
+#define STRINGUTILS_H
+
+#include <string>
+#include <vector>
+#include <sstream>
+
+class StringUtils{
+	private:
+	std::string data;
+	std::string currentIndexOf;
+	public:
+	
+	~StringUtils();
+	StringUtils(std::string const& data);
+	std::vector<std::string> const& split(std::string const& delimiter);
+	std::size_t indexOf(std::string const& input);
+	bool contains(std::string const& input);
+	//std::string const& substring(std::size_t x,std::size_t y);
+	std::string const& substring(std::string const& x,std::string const& y);
+	std::string const& replace(std::string const& input,std::string const& output);
+	
+	template <class T>
+	static std::string const& toString(T const& object){
+		std::stringstream ss;
+    	ss << object;
+    	return ss.str();
+	}
+};
+
+#endif
