3f5ba0fa3376c89ca14b84b5e2e92ffdb54755df
ISAAC300 <isaacsantosdsantos@gmail.com>
2017-03-04 09:15:01 -0400

    Update Android.mk
    added to be executed "Parameter.cpp"

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_CPP_EXTENSION := .cpp .cc
 
-LOCAL_SRC_FILES = main.cpp interpreter/Interpreter.cpp utils/StringUtils.cpp
+LOCAL_SRC_FILES = main.cpp interpreter/Interpreter.cpp utils/StringUtils.cpp interpreter/Parameter.cpp
 
 LOCAL_MODULE := interpreterCpp
 
